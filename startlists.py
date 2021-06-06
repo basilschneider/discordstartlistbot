fe8d968ddd29f7ee50bd0095ee0d660ddda6b2dd
Basil Schneider <basil.schneider@bluewin.ch>
2021-06-06 15:15:08 +0200

    Update to TDS

--- startlists.py
+++ startlists.py
@@ -68,6 +68,6 @@ async def get_startlist(collectionname, racename, ctx, *args):
 #    await get_startlist('2021_gent-wevelgem', 'GW', ctx, *args)
 @bot.command(name='sl')
 async def sl(ctx, *args):
-    await get_startlist('2021_dauphine', 'Dauphine', ctx, *args)
+    await get_startlist('2021_tds', 'TDS', ctx, *args)
 
 bot.run(TOKEN)
